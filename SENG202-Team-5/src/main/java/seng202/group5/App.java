d7d14ec8e026d6913d7106f60c08ba6d461dbf91
Name <mmo155@uclive.ac.nz>
2019-08-02 11:53:46 +1200

    Creating Maven

---
+++ SENG202-Team-5/src/main/java/seng202/group5/App.java
@@ -0,0 +1,13 @@
+package seng202.group5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
