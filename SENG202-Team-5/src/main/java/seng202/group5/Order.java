249af434c30d46fd668c3dc56aca471c96b6b1a8
Michael Morgoun <mmo155@uclive.ac.nz>
2019-08-15 15:48:50 +1200

    Created the skeleton code of the Order class.

---
+++ SENG202-Team-5/src/main/java/seng202/group5/Order.java
@@ -0,0 +1,54 @@
+package seng202.group5;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author Michael Morgoun
+ */
+public class Order {
+
+
+    private HashMap<MenuItem, Integer> orderItems; // A HashMap of items in the order and their quantites.
+    private float totalCost = 0; // The current total cost of the order.
+    String iD; // The unique ID of the order given by the database.
+
+
+    /**
+     * Removes the item taken as a parameter from the order and returns a boolean true or false as to whether
+     * it was successful or not.
+     * @param item
+     * @return
+     */
+    public boolean removeItem(MenuItem item) {
+
+    }
+
+
+    /**
+     * modifies the quantity of an item taken in as a parameter already in the order with the quantity parameter.
+     * Returns a boolean true or false as to whether it was successful or not.
+     * @param item
+     * @param quantity
+     * @return
+     */
+    public boolean modifyItemQuantity(MenuItem item, int quantity) {
+
+    }
+
+
+    /**
+     * Gives a discount to the order based off a percentage given as a parameter.
+     * @param percentage
+     */
+    public void discount(int percentage) {
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
