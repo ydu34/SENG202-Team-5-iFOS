80fd0bbad6b20c08c02322535dd9a616989eb901
Tasman Berry <tjb142@uclive.ac.nz>
2019-08-20 01:02:49 +1200

    Fixed documentation

--- SENG202-Team-5/src/main/java/seng202/group5/Finance.java
+++ SENG202-Team-5/src/main/java/seng202/group5/Finance.java
@@ -18,23 +18,25 @@ public class Finance {
      * Refunds a previous order and returns the list of notes to return in descending size order.
      *
      * @param ID the id of the order to refund
-     * @return a list of floats representing money in descending size order
+     * @return a list of doubles representing money in descending size order
      *
      */
     public ArrayList<Double> refund(String ID) {
-        return new ArrayList<Double>();
+        return new ArrayList<>();
     }
 
     /**
      * Saves order to database and returns a list of notes to return as change.
      *
      * @param totalCost the total cost of the order
-     * @param amountPayed a lost of floats representing money in the denominations payed
+     * @param amountPayed a lost of doubles representing money in the denominations payed
      * @param time the time the order occurred at
-     * @return a list of floats representing money in descending size order
+     * @return a list of doubles representing money in descending size order
+     * @throws InsufficientCashException
      */
     public ArrayList<Double> pay(double totalCost, ArrayList<Double> amountPayed, int time) {
-        return new ArrayList<Double>();
+        throw InsufficientCashException;
+        return new ArrayList<>();
     }
 
     /**
@@ -42,9 +44,9 @@ public class Finance {
      *
      * @param startDate the first date to search from
      * @param endDate the last date to search to
-     * @return a list of float representing  total profits, average profits, and other things
+     * @return a list of doubles representing  total profits, average profits, and other things
      */
     public ArrayList<Double> totalCalculator(int startDate, int endDate) {
-        return new ArrayList<Double>();
+        return new ArrayList<>();
     }
 }
