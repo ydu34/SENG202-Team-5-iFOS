f53172e1b5e34374f22227fc35c3ffe221998c3f
James <youremail@uclive.ac.nz>
2019-08-25 15:02:47 +1200

    Created InsufficientCashException()
    Signed-off-by: James <youremail@uclive.ac.nz>

--- SENG202-Team-5/src/main/java/seng202/group5/exceptions/InsufficientCashException.java
+++ SENG202-Team-5/src/main/java/seng202/group5/exceptions/InsufficientCashException.java
@@ -1,5 +1,11 @@
 package seng202.group5.exceptions;
 
 public class InsufficientCashException extends Exception {
+    public InsufficientCashException() {
 
+    }
+
+    public InsufficientCashException(String message) {
+        super(message);
+    }
 }
