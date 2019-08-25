916ae52c734b68620d1aa56fb6c8670450d99845
James <youremail@uclive.ac.nz>
2019-08-25 15:05:45 +1200

    Created NoOrderException()
    Signed-off-by: James <youremail@uclive.ac.nz>

--- SENG202-Team-5/src/main/java/seng202/group5/exceptions/NoOrderException.java
+++ SENG202-Team-5/src/main/java/seng202/group5/exceptions/NoOrderException.java
@@ -1,5 +1,11 @@
 package seng202.group5.exceptions;
 
 public class NoOrderException extends Exception {
+    public NoOrderException() {
 
+    }
+
+    public NoOrderException(String message) {
+        super(message);
+    }
 }
