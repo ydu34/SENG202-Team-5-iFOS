a963204419ef7df1092b9ecf9b006fea36f85153
Tasman Berry <tjb142@uclive.ac.nz>
2019-08-26 13:08:41 +1200

    Updated tests to use Joda money and added a test for negative cost orders

--- SENG202-Team-5/src/test/java/seng202/group5/FinanceTest.java
+++ SENG202-Team-5/src/test/java/seng202/group5/FinanceTest.java
@@ -1,6 +1,7 @@
 package seng202.group5;
 
 import junit.framework.TestCase;
+import org.joda.money.Money;
 import org.junit.Ignore;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -25,54 +26,65 @@ public class FinanceTest extends TestCase {
     }
 
     @Test
-    public void testPay() {
-        ArrayList<Double> payed = new ArrayList<>();
-        payed.add(10.00);
-        payed.add(5.00);
-        payed.add(5.00);
-        ArrayList<Double> result;
-        result = testFinance.pay(16.75, payed, 300);
-        ArrayList<Double> expectedResult = new ArrayList<>();
-        expectedResult.add(2.00);
-        expectedResult.add(1.00);
-        expectedResult.add(0.20);
+    public void testPay() throws InsufficientCashException {
+        ArrayList<Money> payed = new ArrayList<>();
+        payed.add(Money.parse("NZD 10.00"));
+        payed.add(Money.parse("NZD 5.00"));
+        payed.add(Money.parse("NZD 5.00"));
+        ArrayList<Money> result;
+        result = testFinance.pay(Money.parse("NZD 16.75"), payed, 300);
+        ArrayList<Money> expectedResult = new ArrayList<>();
+        expectedResult.add(Money.parse("NZD 2.00"));
+        expectedResult.add(Money.parse("NZD 1.00"));
+        expectedResult.add(Money.parse("NZD 0.20"));
         assertEquals(result, expectedResult);
     }
 
     @Test
-    public void testPayRounding() {
-        ArrayList<Double> payed = new ArrayList<>();
-        payed.add(10.00);
-        payed.add(5.00);
-        payed.add(5.00);
-        ArrayList<Double> result;
-        result = testFinance.pay(19.01, payed, 300);
-        ArrayList<Double> expectedResult = new ArrayList<>();
-        expectedResult.add(1.00);
+    public void testPayRounding() throws InsufficientCashException {
+        ArrayList<Money> payed = new ArrayList<>();
+        payed.add(Money.parse("NZD 10.00"));
+        payed.add(Money.parse("NZD 5.00"));
+        payed.add(Money.parse("NZD 5.00"));
+        ArrayList<Money> result;
+        result = testFinance.pay(Money.parse("NZD 19.01"), payed, 300);
+        ArrayList<Money> expectedResult = new ArrayList<>();
+        expectedResult.add(Money.parse("NZD 1.00"));
         assertEquals(result, expectedResult);
     }
     @Test
-    public void testPayError() {
-        ArrayList<Double> payed = new ArrayList<>();
-        payed.add(10.00);
-        payed.add(5.00);
-        payed.add(5.00);
+    public void testPaymentError() {
+        ArrayList<Money> payed = new ArrayList<>();
+        payed.add(Money.parse("NZD 10.00"));
+        payed.add(Money.parse("NZD 5.00"));
+        payed.add(Money.parse("NZD 5.00"));
         assertThrows(InsufficientCashException.class, () -> {
-            testFinance.pay(25, payed, 300);
+            testFinance.pay(Money.parse("NZD 25.00"), payed, 300);
             });
 
     }
 
     @Test
+    public void testCostError() {
+        ArrayList<Money> payed = new ArrayList<>();
+        payed.add(Money.parse("NZD 10.00"));
+        payed.add(Money.parse("NZD 5.00"));
+        payed.add(Money.parse("NZD 5.00"));
+        assertThrows(InsufficientCashException.class, () -> {
+            testFinance.pay(Money.parse("NZD -25.00"), payed, 300);
+        });
+
+    }
+    @Test
     public void testRefund() {
-        ArrayList<Double> moneyRefund = new ArrayList<>();
+        ArrayList<Money> moneyRefund = new ArrayList<>();
 
         assertEquals(testFinance.refund(orderID), moneyRefund);
     }
 
     @Test
     public void testTotalCalculator() {
-        ArrayList<Double> total = new ArrayList<>();
+        ArrayList<Money> total = new ArrayList<>();
 
         assertEquals(testFinance.totalCalculator(date1, date2), total);
     }
