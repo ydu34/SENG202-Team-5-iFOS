9c89d9c13124811cba899e8b8cf9f7e4a757a3a6
Tasman Berry <tjb142@uclive.ac.nz>
2019-08-26 13:14:45 +1200

    Updated to use Joda money and implemented most of the pay function

--- SENG202-Team-5/src/main/java/seng202/group5/Finance.java
+++ SENG202-Team-5/src/main/java/seng202/group5/Finance.java
@@ -2,6 +2,7 @@ package seng202.group5;
 
 import java.util.ArrayList;
 
+import org.joda.money.Money;
 import seng202.group5.exceptions.InsufficientCashException;
 
 /**
@@ -24,7 +25,7 @@ public class Finance {
      * @param ID the id of the order to refund
      * @return a list of doubles representing money in descending size order
      */
-    public ArrayList<Double> refund(String ID) {
+    public ArrayList<Money> refund(String ID) {
         return new ArrayList<>();
     }
 
@@ -35,10 +36,18 @@ public class Finance {
      * @param amountPayed a lost of doubles representing money in the denominations payed
      * @param time        the time the order occurred at
      * @return a list of doubles representing money in descending size order
-     * @throws InsufficientCashException
+     * @throws InsufficientCashException Throws error when total cost is negative or the total cost is higher than the amount payed
      */
-    public ArrayList<Double> pay(double totalCost, ArrayList<Double> amountPayed, int time) throws InsufficientCashException {
-        return new ArrayList<>();
+    public ArrayList<Money> pay(Money totalCost, ArrayList<Money> amountPayed, int time) throws InsufficientCashException {
+        Money payedSum = Money.parse("NZD 0");
+        for (Money money: amountPayed)
+        {
+            payedSum = payedSum.plus(money);
+        }
+        if (totalCost.isGreaterThan(payedSum) || totalCost.isNegative()) {
+            throw new InsufficientCashException();
+        }
+        return calcChange(payedSum.minus(totalCost));
     }
 
     /**
@@ -48,8 +57,49 @@ public class Finance {
      * @param endDate   the last date to search to
      * @return a list of doubles representing  total profits, average profits, and other things
      */
-    public ArrayList<Double> totalCalculator(int startDate, int endDate) {
+    public ArrayList<Money> totalCalculator(int startDate, int endDate) {
         return new ArrayList<>();
+    }
+    /**
+     * returns a list containing the change need to be returned
+     *
+     * @param change the first date to search from
+     * @return returns a list containing the change need to be returned
+     */
+    public ArrayList<Money> calcChange(Money change) {
+        ArrayList<Money> totalChange = new ArrayList<>();
+        change.plus(Money.parse("NZD 0.03"));
+        while (change.isGreaterThan(Money.parse("NZD 0.09"))) {
+            if (change.isGreaterThan(Money.parse("NZD 50.00"))) {
+                totalChange.add(Money.parse("NZD 50.00"));
+                change.minus(Money.parse("NZD 50.00"));
+            }else if (change.isGreaterThan(Money.parse("NZD 20.00"))) {
+                totalChange.add(Money.parse("NZD 20.00"));
+                change.minus(Money.parse("NZD 20.00"));
+            }else if (change.isGreaterThan(Money.parse("NZD 10.00"))) {
+                totalChange.add(Money.parse("NZD 10.00"));
+                change.minus(Money.parse("NZD 10.00"));
+            }else if (change.isGreaterThan(Money.parse("NZD 5.00"))) {
+                totalChange.add(Money.parse("NZD 5.00"));
+                change.minus(Money.parse("NZD 5.00"));
+            }else if (change.isGreaterThan(Money.parse("NZD 2.00"))) {
+                totalChange.add(Money.parse("NZD 2.00"));
+                change.minus(Money.parse("NZD 2.00"));
+            }else if (change.isGreaterThan(Money.parse("NZD 1.00"))) {
+                totalChange.add(Money.parse("NZD 1.00"));
+                change.minus(Money.parse("NZD 1.00"));
+            }else if (change.isGreaterThan(Money.parse("NZD 0.50"))) {
+                totalChange.add(Money.parse("NZD 0.50"));
+                change.minus(Money.parse("NZD 0.50"));
+            }else if (change.isGreaterThan(Money.parse("NZD 0.20"))) {
+                totalChange.add(Money.parse("NZD 0.20"));
+                change.minus(Money.parse("NZD 0.20"));
+            }else {
+                totalChange.add(Money.parse("NZD 0.10"));
+                change.minus(Money.parse("NZD 0.10"));
+            }
+        }
+        return totalChange;
     }
 
 }
