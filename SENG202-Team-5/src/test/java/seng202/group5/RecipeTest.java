cf45ad8590f4f11251f229e732805c8664ab33fd
Daniel Harris <djh211@uclive.ac.nz>
2019-08-27 15:42:33 +1200

    Made some changes to RecipeTest to comply with previous change to Recipe. Also made some name changes to conform to test name style

--- SENG202-Team-5/src/test/java/seng202/group5/RecipeTest.java
+++ SENG202-Team-5/src/test/java/seng202/group5/RecipeTest.java
@@ -8,14 +8,14 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class RecipeTesting
+public class RecipeTest
         extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public RecipeTesting(String testName) {
+    public RecipeTest(String testName) {
         super(testName);
     }
 
@@ -23,32 +23,32 @@ public class RecipeTesting
      * @return the suite of tests being tested
      */
     public static Test suite() {
-        return new TestSuite(RecipeTesting.class);
+        return new TestSuite(RecipeTest.class);
     }
 
-    public void testAddingredient() {
+    public void testAddIngredient() {
 
         Recipe testRecipe = new Recipe("Vege burger", "Steps to make pad thai", (float) 2.0);
-        Ingredient chickenpatty = new Ingredient("chicken", 2, "meat", 12, 20, true, false, false);
+        Ingredient chickenpatty = new Ingredient("chicken", "kg", "meat", "12", 20, true, false, false);
         testRecipe.addIngredient(chickenpatty, 1);
         assertTrue(testRecipe.getGlutenfree());
         assertFalse(testRecipe.getVegetarian());
         assertFalse(testRecipe.getVegan());
     }
 
-    public void testRemovingredient() {
+    public void testRemoveIngredient() {
 
         Recipe testRecipe = new Recipe("Vege burger", "Steps to make pad thai", (float) 2.0);
-        Ingredient chickenPatty = new Ingredient("chicken", 2, "meat", 15, 20, true, false, false);
+        Ingredient chickenPatty = new Ingredient("chicken", "kg", "meat", "12", 20, true, false, false);
         testRecipe.removeIngredient(chickenPatty, 1);
         assertTrue(testRecipe.getVegetarian());
         assertTrue(testRecipe.getGlutenfree());
     }
 
-    public void testEditingredient() {
+    public void testEditIngredient() {
 
         Recipe testRecipe = new Recipe("Vege burger", "Steps to make pad thai", (float) 2.0);
-        Ingredient chickenPatty = new Ingredient("chicken", 2, "meat", 15, 20, true, false, false);
+        Ingredient chickenPatty = new Ingredient("chicken", "kg", "meat", "12", 20, true, false, false);
         testRecipe.addIngredient(chickenPatty, 10);
         assertTrue(testRecipe.editRecepie(chickenPatty, 5));
     }
