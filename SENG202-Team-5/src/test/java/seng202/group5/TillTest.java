4943f0faca0109a253593bd70fb636ab249d0c62
James <jtk39@uclive.ac.nz>
2019-08-28 18:14:53 +1200

    Added 2 test cases for adding and removing denomations in the TillTest JUnit class.
    Signed-off-by: James Kwok <jtk39@uclive.ac.nz>

--- SENG202-Team-5/src/test/java/seng202/group5/TillTest.java
+++ SENG202-Team-5/src/test/java/seng202/group5/TillTest.java
@@ -1,7 +1,10 @@
 package seng202.group5;
 
 import org.joda.money.Money;
+import org.junit.Test;
 import org.junit.jupiter.api.*;
+
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,9 +13,38 @@ class TillTest {
 
     private Till testTill;
     @BeforeEach
-    Money
-    testTill = new Till()
+    public void init() {
+    Money testMoney10 = null;
+    Money testMoney20 = null;
+    Money testMoney50 = null;
+    testMoney10.parse("NZD 10.00");
+    testMoney20.parse("NZD 20.00");
+    testMoney50.parse("NZD 50.00");
+    HashMap<Money, Integer> testDenominations = new HashMap<Money, Integer>();
+    testTill = new Till(testDenominations);
+
+
     }
 
+    @Test
+    public testAddNewDenomination() {
+        Money testMoney30 = null;
+        testMoney30.parse("NZD 30.00");
+        testTill.newDenomination(testMoney30);
+        assertEquals(4, testTill.getDenominations().size());
+    }
 
+    @Test
+    public testRemoveDenomination() {
+        Money testMoney30 = null;
+        testMoney30.parse("NZD 30.00");
+        testTill.newDenomination(testMoney30);
+        assertEquals(4, testTill.getDenominations().size());
+        testTill.removeDenomination(testMoney20);
+        assertEquals(3, testTill.getDenominations().size());
+
+    }
+
+    @Test
+    public
 }
