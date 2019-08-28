d32dc26e28d99723e5d53d24524b61bd57e2d91a
Yu Duan <ydu34@uclive.ac.nz>
2019-08-28 15:18:59 +1200

    Completed working on the MenuItemHandler, the method should be able to unmarshall and marshall menu.xml, which is the xml file containing the hashmap in MenuManager. The class might need to be revisited.

--- SENG202-Team-5/src/main/java/handlers/MenuItemHandler.java
+++ SENG202-Team-5/src/main/java/handlers/MenuItemHandler.java
@@ -1,41 +1,55 @@
 package handlers;
 
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
+import seng202.group5.MenuManager;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 import java.io.File;
-import java.io.IOException;
 
 
 //storing history and datatype stuff. menuitem and stock vs transactions and orders should stay inside at same time, not going to change overtime
 
 //ideal plan; have a superclass which can open the data, then have functions in each handler which works with the XML format specified.
 
-
+/**
+ *
+ */
 public class MenuItemHandler {
 
+    /**Converts the hashmap menu contained in MenuManager to an xml file named menu.xml.
+     * @param menuManager a instance of the class MenuManager to have it's menu map converted to menu.xml.
+     */
+    public void menuToXml(MenuManager menuManager) {
 
-    //*************** DOM STUFF ****************
-    public void getDataXML(String filename, boolean validating) throws ParserConfigurationException, IOException, SAXException {
-        //        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        //        DocumentBuilder builder = null;
-        //        factory.setValidating(validating);
-        //        try {
-        //            builder = factory.newDocumentBuilder();
-        //        } catch (ParserConfigurationException e) {
-        //            System.err.println(e);
-        //            System.exit();
-        //        }
-        //        //builder.setErrorHandler(new ); new myerrorhandler
-        //        Document document = builder.parse(new File(filename));
-        //        System.out.println(document);
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(MenuManager.class);
+            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+
+            jaxbMarshaller.marshal(menuManager, System.out); //print to sys out so we can view and check
+            jaxbMarshaller.marshal(menuManager, new File(System.getProperty("user.dir") +
+                                                                 "/src/resources/data/menu.xml"));
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void main(String[] args) {
-        //getDataXML('')
+    /**Converts the xml file menu.xml containing the menuitems into a hashmap contained in the class MenuManager.
+     * @return a instance of the class MenuManager with the map filled up with the MenuItems in the menu.xml.
+     */
+    public MenuManager xmlToMenu() {
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(MenuManager.class);
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            MenuManager menuManager = (MenuManager) jaxbUnmarshaller.unmarshal( new File(System.getProperty("user.dir") +
+                                                                                                 "/src/resources/data/menu.xml"));
+            return menuManager;
+
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
