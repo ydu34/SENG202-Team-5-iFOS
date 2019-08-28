d4a2e33c1a84bb03510559d1799cbb21167a3056
James <jtk39@uclive.ac.nz>
2019-08-28 17:57:37 +1200

    Created TillTest class for testing the Till class.
    Signed-off-by: James Kwok <jtk39@uclive.ac.nz>

---
+++ SENG202-Team-5/src/test/java/seng202/group5/TillTest.java
@@ -0,0 +1,18 @@
+package seng202.group5;
+
+import org.joda.money.Money;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TillTest {
+
+
+    private Till testTill;
+    @BeforeEach
+    Money
+    testTill = new Till()
+    }
+
+
+}
