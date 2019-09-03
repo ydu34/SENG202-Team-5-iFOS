2ecfc3e9620cbcdf7ce60644d8189237ed39cd05
James <jtk39@uclive.ac.nz>
2019-09-03 13:40:24 +1200

    Reverted changes to MenuManager, fixed constructor in Recipe to create the HashMap properly. FIxed MenuManagerTest to use JUnit Jupiter.
    Signed-off-by: James Kwok <jtk39@uclive.ac.nz>

--- SENG202-Team-5/src/test/java/seng202/group5/MenuManagerTest.java
+++ SENG202-Team-5/src/test/java/seng202/group5/MenuManagerTest.java
@@ -1,58 +1,60 @@
 package seng202.group5;
 
-import junit.framework.TestCase;
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class MenuManagerTest {
     private Map<String, MenuItem> itemList = new HashMap<String, MenuItem>();
-    private MenuManager menuManager;
-    private String burgerRecipeText;
-    private HashMap<Ingredient, Integer> ingredient;
+    private MenuManager menuManager = new MenuManager();
+    private String burgerRecipeText = "Make a burger!";
+    private Ingredient bun = new Ingredient("Bun", "300", "Main", "Bun123", 10.30);
+    private HashMap<Ingredient, Integer> ingredients = new HashMap<Ingredient, Integer>();
+    private Worker worker;
 
 
 
     @BeforeEach
     public void init() {
-        String burgerRecipeText = "Make a burger!";
-        HashMap<Ingredient, Integer> ingredient = new HashMap<>();
+        HashMap<Ingredient, Integer> ingredients = new HashMap<>();
     }
 
-//    @Test
-//    public void testCreateRecipeReturnsRecipe() {
-//        Recipe burger = menuManager.createRecipe(ingredient, burgerRecipeText);
-//        assertTrue(burger instanceof Recipe);
-//    }
-//
-//    @Test
-//    public void testCreateItemAddtoMenu() {
-//        Recipe burger = menuManager.createRecipe(ingredient, burgerRecipeText);
-//        String name = "cheeseBurger";
-//        double cost = 11.50f;
-//        boolean inMenu = true;
-//        menuManager.createItem(name, burger, cost, inMenu);
-//        assertTrue(menuManager.getItemList().size() == 1);
-//    }
-//
-//    @Test
-//    public void testCreateItemNotAddtoMenu() {
-//        Recipe burger = menuManager.createRecipe(ingredient, burgerRecipeText);
-//        String name = "cheeseBurger";
-//        double cost = 11.50f;
-//        boolean inMenu = false;
-//        menuManager.createItem(name, burger, cost, inMenu);
-//        assertTrue(menuManager.getItemList().size() == 0);
-//    }
+    @Test
+    public void testCreateRecipeReturnsRecipe() {
 
-    @Disabled
+        ingredients.put(bun, 2);
+        Recipe burger = menuManager.createRecipe("Burger", ingredients, burgerRecipeText);
+        assertTrue(burger instanceof Recipe);
+    }
+
+    @Test
+    public void testCreateItemAddtoMenu() {
+        ingredients.put(bun, 2);
+        Recipe burger = menuManager.createRecipe("Burger", ingredients, burgerRecipeText);
+        String name = "cheeseBurger";
+        double cost = 11.50f;
+        menuManager.createItem(worker, name, burger, cost, "burg123", true);
+        assertTrue(menuManager.getItemList().size() == 1);
+    }
+
+    @Test
+    public void testCreateItemNotAddtoMenu() {
+        ingredients.put(bun, 2);
+        Recipe burger = menuManager.createRecipe("Burger", ingredients, burgerRecipeText);
+        String name = "cheeseBurger";
+        double cost = 11.50f;
+        boolean inMenu = false;
+        menuManager.createItem(worker, name, burger, cost, "burg123", inMenu);
+        assertTrue(menuManager.getItemList().size() == 1);
+        assertTrue(worker.getMenuItems().size() == 0);
+    }
+
     @Test
     public void testRemoveItemWithItemInMenu() {
         String ID = "B1";
         menuManager.removeItem(ID);
-        //assertTrue(menuManager.getItemList().size() == 0);
+        assertTrue(menuManager.getItemList().size() == 0);
     }
 }
