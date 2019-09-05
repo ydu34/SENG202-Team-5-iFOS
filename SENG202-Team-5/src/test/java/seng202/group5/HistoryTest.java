d000d1980ab65a336ba2ed0829fce5e8141c4b4e
Yu Duan <ydu34@uclive.ac.nz>
2019-09-05 21:39:03 +1200

    Commented out unfinished and broken tests.

--- SENG202-Team-5/src/test/java/seng202/group5/HistoryTest.java
+++ SENG202-Team-5/src/test/java/seng202/group5/HistoryTest.java
@@ -1,42 +1,44 @@
-package seng202.group5;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-
-import java.util.HashMap;
-
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-
-class HistoryTest {
-    private History history;
-
-    @BeforeEach
-    void init() {
-        HashMap<String, Order> tempTransactionHistory = new HashMap<>();
-        History history = new History(tempTransactionHistory);
-        double tempTotalCost = 0;
-        String tempID = "1";
-        Order order = new Order(tempOrderItems, tempTotalCost, tempID);
-    }
-
-    @Test
-    void testViewReturnsOrder() {
-        HashMap<MenuItem, Integer> tempOrderItems = new HashMap<>();
-
-        history.addTransactionHistory();
-        String orderID = "1002";
-
-        Order order = history.view(orderID);
-        assertTrue(order instanceof Order);
-    }
-
-    @Test
-    void testViewOrderDoesNotExist() {
-        String orderID = "1000";
-        Order order = history.view(orderID);
-        assertNull(order);
-    }
-}
+//package seng202.group5;
+//
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Disabled;
+//import org.junit.jupiter.api.Test;
+//
+//import java.util.HashMap;
+//
+//import static org.junit.jupiter.api.Assertions.assertNull;
+//import static org.junit.jupiter.api.Assertions.assertTrue;
+//
+//
+//class HistoryTest {
+//    private History history;
+//
+//    @BeforeEach
+//    void init() {
+//        HashMap<String, Order> tempTransactionHistory = new HashMap<>();
+//        HashMap<MenuItem, Integer> tempOrderItems = new HashMap<MenuItem, Integer>();
+//        History history = new History(tempTransactionHistory);
+//        double tempTotalCost = 0;
+//        String tempID = "1";
+//
+//        Order order = new Order(tempOrderItems, tempTotalCost, tempID);
+//    }
+//
+//    @Test
+//    void testViewReturnsOrder() {
+//        HashMap<MenuItem, Integer> tempOrderItems = new HashMap<>();
+//
+//        history.addTransactionHistory();
+//        String orderID = "1002";
+//
+//        Order order = history.view(orderID);
+//        assertTrue(order instanceof Order);
+//    }
+//
+//    @Test
+//    void testViewOrderDoesNotExist() {
+//        String orderID = "1000";
+//        Order order = history.view(orderID);
+//        assertNull(order);
+//    }
+//}
