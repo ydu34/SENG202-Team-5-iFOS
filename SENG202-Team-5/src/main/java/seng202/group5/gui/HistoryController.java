6d4a7ba300c7b16ce927f49823c015989acf50fb
Daniel Harris <djh211@uclive.ac.nz>
2019-09-09 23:27:08 +1200

    Added remaining elements to the History screen, and restricted date selection

--- SENG202-Team-5/src/main/java/seng202/group5/gui/HistoryController.java
+++ SENG202-Team-5/src/main/java/seng202/group5/gui/HistoryController.java
@@ -1,7 +1,7 @@
 package seng202.group5.gui;
 
 /**
- * author @ Shivin Gaba
+ * @author Shivin Gaba, Daniel Harris
  */
 
 
@@ -12,9 +12,14 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.DateCell;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.util.converter.LocalDateStringConverter;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 public class HistoryController {
 
@@ -29,6 +34,15 @@ public class HistoryController {
 
     @FXML
     private Button launchHistoryButton;
+
+    @FXML
+    private DatePicker historyStartDatePicker;
+
+    @FXML
+    private DatePicker historyEndDatePicker;
+
+    @FXML
+    private TextField historySearchbar;
 
     /**
      * This method is called when any of the button (invoice, history,stock or admin are clicked on the order the screen)
@@ -81,10 +95,103 @@ public class HistoryController {
      * This method launches the stock screen when clicked on the "History" button
      * @param actionEvent
      */
-
-
     public void launchStockScreen(javafx.event.ActionEvent actionEvent) {
         changeScreen(actionEvent, "/gui/stock.fxml");
     }
+
+
+    /**
+     * Sets the DateCell creators for the start date picker
+     *
+     * @param event
+     */
+    public void setStartDateUpdater(javafx.event.Event event) {
+        historyStartDatePicker.setDayCellFactory(picker -> new DateCell() {
+            @Override
+            public void updateItem(LocalDate date, boolean empty) {
+                super.updateItem(date, empty);
+                LocalDate endDate = historyEndDatePicker.getValue();
+                if (endDate == null) {
+                    setDisable(empty);
+                } else {
+                    setDisable(empty || date.compareTo(endDate) > 0);
+                }
+            }
+        });
+        historyStartDatePicker.setConverter(new LocalDateStringConverter() {
+            @Override
+            public LocalDate fromString(String input) {
+                LocalDate date = super.fromString(input);
+                LocalDate endDate = historyEndDatePicker.getValue();
+                if (endDate != null && date.compareTo(endDate) > 0) {
+                    date = endDate;
+                }
+                return date;
+            }
+        });
+    }
+
+    /**
+     * Sets the DateCell creators for the end date picker
+     *
+     * @param event
+     */
+    public void setEndDateUpdater(javafx.event.Event event) {
+        historyEndDatePicker.setDayCellFactory(picker -> new DateCell() {
+            @Override
+            public void updateItem(LocalDate date, boolean empty) {
+                super.updateItem(date, empty);
+                LocalDate startDate = historyStartDatePicker.getValue();
+                if (startDate == null) {
+                    setDisable(empty);
+                } else {
+                    setDisable(empty || date.compareTo(startDate) < 0);
+                }
+            }
+        });
+        historyEndDatePicker.setConverter(new LocalDateStringConverter() {
+            @Override
+            public LocalDate fromString(String input) {
+                LocalDate date = super.fromString(input);
+                LocalDate startDate = historyStartDatePicker.getValue();
+                if (startDate != null && date.compareTo(startDate) < 0) {
+                    date = startDate;
+                }
+                return date;
+            }
+        });
+    }
+
+    /**
+     * Iterates through the available end dates and sets which are selectable depending on the start date
+     *
+     * @param actionEvent
+     */
+    public void updateSelectableEndDates(javafx.event.ActionEvent actionEvent) {
+        for (Node element : historyEndDatePicker.getChildrenUnmodifiable()) {
+            if (element instanceof DateCell) {
+                DateCell dateElement = (DateCell) element;
+                dateElement.updateItem(dateElement.getItem(), dateElement.isEmpty());
+            }
+        }
+        //TODO add code to add and remove orders from the table in a separate function
+    }
+
+    /**
+     * Iterates through the available start dates and sets which are selectable depending on the end date
+     *
+     * @param actionEvent
+     */
+    public void updateSelectableStartDates(javafx.event.ActionEvent actionEvent) {
+        for (Node element : historyStartDatePicker.getChildrenUnmodifiable()) {
+            if (element instanceof DateCell) {
+                DateCell dateElement = (DateCell) element;
+                dateElement.updateItem(dateElement.getItem(), dateElement.isEmpty());
+            }
+        }
+        //TODO add code to add and remove orders from the table in a separate function
+    }
+
+
 
 }
