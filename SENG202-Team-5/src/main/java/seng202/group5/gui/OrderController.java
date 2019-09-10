7b5fd851299dcfef064d5781395baa536b6b0062
Shivin Gaba <sga113@uclive.ac.nz>
2019-09-10 21:15:05 +1200

    Try implementing the check boxes on order screen. Added some functionality to the the confirm button and showItems() method in the OrderController.

--- SENG202-Team-5/src/main/java/seng202/group5/gui/OrderController.java
+++ SENG202-Team-5/src/main/java/seng202/group5/gui/OrderController.java
@@ -1,5 +1,3 @@
-
-
 package seng202.group5.gui;
 
 import javafx.event.ActionEvent;
@@ -9,6 +7,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -19,6 +18,7 @@ import seng202.group5.MenuItem;
 import seng202.group5.Recipe;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Map;
 
@@ -49,36 +49,86 @@ public class OrderController {
     @FXML
     private Button itemButton;
     private MenuItem item;
+    private MenuItem item2;
+    @FXML
+    private Recipe testRecipe2;
     @FXML
     private Text totalCostDisplay;
     private String ingredient;
+    @FXML
+    private CheckBox vegan;
+
+    @FXML
+    private CheckBox vegetarian;
+
+    @FXML
+    private CheckBox glutenFree;
+
+
+
+    private ArrayList<MenuItem> menuItemsList =new ArrayList<MenuItem>();
+
+    public void checkDietryInfo(ActionEvent event) {
+        // get values of checkboxes here
+        // call showItems with those vlaues as parameter
+    }
+
+    /**
+     * Still trying to work this code out. Please dont delete it.
+     */
+    public void showItems() {
+        // work with menuItemsList
+        make_object();
+        System.out.println(glutenFree.isSelected());
+        for(MenuItem i : menuItemsList){
+            if(i.getRecipe().getDietaryInformation().contains(DietEnum.GLUTEN_FREE) && glutenFree.isSelected()){
+                System.out.println("print its GF");
+            }
+        }
+
+            }
+
 
     /**
      * this method is juts a test as we are still trying to figure out how to convert the menuitem from the recipe xml to the recipe object.
-     * The method below adds makes a recipe object, adds ingredients to it and the loop over the hash map <Ingredient, Integer> and displays the
+     * The method below adds makes a menuItem object, adds ingredients to it and the loop over the hash map <Ingredient, Integer> and displays the
      * the list of all the ingredients present in that recipe under on the order screen under the ingredients method and the also return the recipe.
      */
-    public Recipe make_object() {
+    public ArrayList<MenuItem> make_object() {
 
         ingredient = "";
-        testRecipe = new Recipe("Vege burger", "Steps to make pad thai");
-        item = new MenuItem("Vege BUrger",testRecipe, Money.parse("NZD 5"), "1221", true);
-        HashSet<DietEnum> ingredientInfo = new HashSet<>() {{
+        testRecipe = new Recipe("Chicken burger", "Steps to make pad thai");
+        testRecipe2 = new Recipe("Vege burger", "Steps to make pad thai");
+        item = new MenuItem("Chicken Burger",testRecipe, Money.parse("NZD 5"), "1221", true);
+        item2 = new MenuItem("Vege Burger",testRecipe2, Money.parse("NZD 7"), "1222", true);
+        HashSet<DietEnum> ingredientInfo1 = new HashSet<>() {{
             add(DietEnum.GLUTEN_FREE);
         }};
-        Ingredient chickenpatty = new Ingredient("chicken", "kg", "meat", "12", Money.parse("NZD 10"), ingredientInfo);
-        Ingredient cheese = new Ingredient("cheese", "kg", "dairy", "12", Money.parse("NZD 5"), ingredientInfo);
+        HashSet<DietEnum> ingredientInfo2 = new HashSet<>() {{
+            add(DietEnum.GLUTEN_FREE);
+          //  add(DietEnum.VEGETARIAN);
+        }};
+        Ingredient chickenpatty = new Ingredient("chicken", "kg", "meat", "12", Money.parse("NZD 10"), ingredientInfo1);
+        Ingredient cheese = new Ingredient("cheese", "kg", "dairy", "12", Money.parse("NZD 5"), ingredientInfo2);
+        HashSet<DietEnum> ingredientInfo3 = new HashSet<>() {{
+            add(DietEnum.GLUTEN_FREE);
+           // add(DietEnum.VEGETARIAN);
+        }};
+        Ingredient vegePatty = new Ingredient("vegetables", "kg", "vege", "12", Money.parse("NZD 10"), ingredientInfo3);
         testRecipe.addIngredient(chickenpatty, 1);
         testRecipe.addIngredient(cheese, 1);
+        testRecipe2.addIngredient(cheese, 1);
+        menuItemsList.add(item);
+        menuItemsList.add(item2);
+        // refactor into another method
         for (Map.Entry<Ingredient, Integer> entry : testRecipe.getIngredientsAmount().entrySet()) {
             Ingredient ingredientObject = entry.getKey();
             Integer value = entry.getValue();
             ingredient += ingredientObject.getName() + "(" + value + ")\n";
         }
-        return testRecipe;
+        return menuItemsList;
 
     }
-
 
     /**
      * This method launches the selection screen for the selected menu item and passes the recipe and object from the
@@ -166,7 +216,16 @@ public class OrderController {
     public void getIngredients() {
         make_object();
         ingredientText.setText(ingredient);
+        if(item.getRecipe().getDietaryInformation().contains(DietEnum.GLUTEN_FREE)){
+            System.out.println("print its vegetarian");
+        }
+    //  System.out.println(item.calculateFinalCost().getAmount());
+
+        System.out.println(item.calculateFinalCost().getAmount());
+        System.out.println(String.valueOf(item.calculateFinalCost().getAmount()));
         totalCostDisplay.setText(String.valueOf(item.calculateFinalCost().getAmount()));
+
+
     }
 
 
@@ -178,6 +237,4 @@ public class OrderController {
     public void launchSelectionScreen(javafx.event.ActionEvent actionEvent) {
         selectionScreen(actionEvent, "/gui/selection.fxml");
     }
-
-
 }
