bbb1f015fe844adf67ba9ba610fd77254b146699
Michael Morgoun <mmo155@uclive.ac.nz>
2019-09-18 02:05:58 +1200

    Started on Stock GUI page, got stuff to show. No quantities yet, trying to figure that one out. And started the add ingredient thing but not finished yet. Will prioritise that so that others can make shit.

---
+++ SENG202-Team-5/src/main/java/seng202/group5/gui/AddStockController.java
@@ -0,0 +1,30 @@
+package seng202.group5.gui;
+
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+public class AddStockController {
+
+    @FXML
+    private Button createButton;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField unitField;
+
+    @FXML
+    private TextField categoryField;
+
+    @FXML
+    private TextField costField;
+
+    @FXML
+    private TextField quantityField;
+
+    @FXML
+    private Label warningLabel;
+
+}
--- SENG202-Team-5/src/main/java/seng202/group5/gui/StockController.java
+++ SENG202-Team-5/src/main/java/seng202/group5/gui/StockController.java
@@ -3,6 +3,9 @@
  */
 package seng202.group5.gui;
 
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,8 +15,11 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import seng202.group5.Ingredient;
 import seng202.group5.Order;
+import seng202.group5.Stock;
 
 import java.io.IOException;
 
@@ -21,7 +27,10 @@ public class StockController extends GeneralController {
 
 
     @FXML
-    private TableView<Order> stockTable;
+    private TableView<Ingredient> stockTable;
+
+    @FXML
+    private TableColumn rowID;
 
     @FXML
     private TableColumn rowIngredient;
@@ -43,5 +52,33 @@ public class StockController extends GeneralController {
 
     @FXML
     private Button removeButton;
+
+    @Override
+    public void pseudoInitialize() {
+        ObservableList<Ingredient> ingredients = FXCollections.observableArrayList(
+                getAppEnvironment().getStock().getIngredients().values()
+        );
+        rowID.setCellValueFactory(new PropertyValueFactory("ID"));
+        rowIngredient.setCellValueFactory(new PropertyValueFactory("name"));
+        rowUnits.setCellValueFactory(new PropertyValueFactory("unit"));
+        rowCategory.setCellValueFactory(new PropertyValueFactory("category"));
+
+        stockTable.setItems(ingredients);
+    }
+
+    @FXML
+    public void addIngredient(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/addStock.fxml"));
+            Parent root = (Parent) loader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Add An Ingredient");
+            stage.setScene(new Scene(root, 600, 200));
+            stage.show();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
