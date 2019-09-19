ca409ed5d709550c016acb08d5c273c18b0c6af6
James Kwok <jtk39@uclive.ac.nz>
2019-09-19 14:34:45 +1200

    Implemented initialiseSelectedIngredients() and initialiseRemainingIngredients() to handle initialisation of the addExtraIngredients tableView.
    Signed-off-by: James Kwok <jtk39@uclive.ac.nz>

--- SENG202-Team-5/src/main/java/seng202/group5/gui/AddExtraIngredientController.java
+++ SENG202-Team-5/src/main/java/seng202/group5/gui/AddExtraIngredientController.java
@@ -1,5 +1,6 @@
 package seng202.group5.gui;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -9,12 +10,18 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import seng202.group5.Ingredient;
 import seng202.group5.MenuItem;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Set;
 
 public class AddExtraIngredientController extends GeneralController {
 
@@ -22,31 +29,64 @@ public class AddExtraIngredientController extends GeneralController {
     private Button launchSelectionScreenButton;
 
     @FXML
+    private Button confirmItemButton;
+
+    @FXML
     private MenuItem selectedItem;
+
+    @FXML
+    private TableView<Ingredient> ingredientsTable;
+
+    @FXML
+    private TableColumn<Ingredient, String> rowID = new TableColumn<>("ID");
+
+    @FXML
+    private TableColumn<Ingredient, String> rowIngredientName = new TableColumn<>("ingredientName");
+
+    @FXML
+    private TableColumn<Ingredient, String> rowQuantity =  new TableColumn<>("quantity");
+
+    @FXML
+    private TableColumn<Ingredient, String> rowCategory = new TableColumn<>("category");
+
+    private Set<Ingredient> selectedIngredientSet;
+
+    private ObservableList<Ingredient> itemIngredients;
 
     @Override
     public void pseudoInitialize() {
+        HashMap<String, Integer> quantities = getAppEnvironment().getStock().getIngredientStock();
+        rowID.setCellValueFactory(new PropertyValueFactory<>("ID"));
+        rowIngredientName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        rowQuantity.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+        rowCategory.setCellValueFactory(new PropertyValueFactory<>("category"));
+        rowQuantity.setCellValueFactory(data -> {
+            int quantity = quantities.get(data.getValue().getID());
+            return new SimpleStringProperty(Integer.toString(quantity));
+        });
 
         initializeSelectedIngredients();
         initializeRemainingIngredients();
+        ingredientsTable.setItems(itemIngredients);
         }
 
-    public void launchSelectionScreen(javafx.event.ActionEvent actionEvent) { // This does not remember the order
-        changeScreen(actionEvent, "/gui/selection.fxml");
-    }
+
 
     /**
      * Updates the given item's ingredients to match what is selected in the GUI and returns to the Order screen.
      */
     public void updateItemIngredients(javafx.event.ActionEvent actionEvent) {
+        //TODO Implement for when the extra ingredients are confirmed.
         System.out.println("Implement me!");
     }
 
     /**
-     * Takes a given MenuItem and adds its ingredients required to the tableview.
+     * Takes the selected MenuItem and adds its ingredients required to the tableview.
      */
     public void initializeSelectedIngredients() {
-        System.out.println("Implement me!");
+        selectedIngredientSet = selectedItem.getRecipe().getIngredientsAmount().keySet();
+        itemIngredients = FXCollections.observableArrayList(
+                selectedIngredientSet);
     }
 
     /**
@@ -54,7 +94,22 @@ public class AddExtraIngredientController extends GeneralController {
      * Furthermore adds ingredients with 0 units left, but prevents the user from adding it to the MenuItem.
      */
     public void initializeRemainingIngredients() {
-        System.out.println("Implement Me!");
+        Collection<Ingredient> allIngredientList = getAppEnvironment().getStock().getIngredients().values();
+        ArrayList<Ingredient> ingredientsNotInTable = new ArrayList<>();
+        for (Ingredient ingredient : allIngredientList) {
+            System.out.println(ingredient);
+            if (!selectedIngredientSet.contains(ingredient)) {
+                itemIngredients.add(ingredient);
+            }
+        }
+    }
+
+    public void setMenuItem(MenuItem newItem){
+        selectedItem = newItem;
+    }
+
+    public void launchSelectionScreen(javafx.event.ActionEvent actionEvent) { // This does not remember the order
+        changeScreen(actionEvent, "/gui/selection.fxml");
     }
 
 }
