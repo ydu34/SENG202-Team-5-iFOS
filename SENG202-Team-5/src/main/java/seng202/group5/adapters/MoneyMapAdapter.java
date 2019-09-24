d1e29c7b043eb82c91775672c40191e78a40ccbe
Yu Duan <ydu34@uclive.ac.nz>
2019-09-24 14:58:51 +1200

    Added some javadoc for the adapters and removed exception that is never thrown.

--- SENG202-Team-5/src/main/java/seng202/group5/adapters/MoneyMapAdapter.java
+++ SENG202-Team-5/src/main/java/seng202/group5/adapters/MoneyMapAdapter.java
@@ -6,10 +6,17 @@ import javax.xml.bind.annotation.adapters.XmlAdapter;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * A class created so that Jaxb can deal with Money Objects
+ */
 public final class MoneyMapAdapter extends XmlAdapter<AdaptedMoneyMap, HashMap<Money, Integer>>{
 
+    /**
+     * @param v Takes in an AdaptedMoneyMap that contains a list of AdaptedMoneyEntry
+     * @return HashMap containing money objects and the quantity of each money
+     */
     @Override
-    public HashMap<Money, Integer> unmarshal(AdaptedMoneyMap v) throws Exception {
+    public HashMap<Money, Integer> unmarshal(AdaptedMoneyMap v){
         List<AdaptedMoneyEntry> adaptedEntries = v.entries;
         HashMap<Money, Integer> map = new HashMap<Money, Integer>(adaptedEntries.size());
         for (AdaptedMoneyEntry adaptedMoneyEntry : adaptedEntries) {
@@ -18,8 +25,12 @@ public final class MoneyMapAdapter extends XmlAdapter<AdaptedMoneyMap, HashMap<M
         return map;
     }
 
+    /**
+     * @param v Takes in an HashMap containing money objects and the quantity of each money
+     * @return AdaptedMoneyMap that contains a list of AdaptedMoneyEntry
+     */
     @Override
-    public AdaptedMoneyMap marshal(HashMap<Money, Integer> v) throws Exception {
+    public AdaptedMoneyMap marshal(HashMap<Money, Integer> v){
         AdaptedMoneyMap adaptedMoneyMap = new AdaptedMoneyMap();
         for(HashMap.Entry<Money, Integer> entry : v.entrySet()) {
             AdaptedMoneyEntry adaptedMoneyEntry = new AdaptedMoneyEntry();
