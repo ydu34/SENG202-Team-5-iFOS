3b78f893e333a86d3a66df18089618bed802f9d6
Yu Duan <ydu34@uclive.ac.nz>
2019-10-06 18:00:41 +1300

    Removed broken implementation

--- SENG202-Team-5/src/main/java/seng202/group5/gui/AdminController.java
+++ SENG202-Team-5/src/main/java/seng202/group5/gui/AdminController.java
@@ -10,7 +10,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.input.SwipeEvent;
 import javafx.scene.text.Text;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
@@ -482,18 +481,6 @@ public class AdminController extends GeneralController {
             spinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(
                     0, 999999, initialValue));
         }
-    }
-
-    @FXML
-    public void swipeLeft(SwipeEvent swipeEvent) {
-        Integer currentTabIndex = adminTabPane.getSelectionModel().getSelectedIndex();
-        adminTabPane.getSelectionModel().select(currentTabIndex--);
-    }
-
-    @FXML
-    public void swipeRight(SwipeEvent swipeEvent) {
-        Integer currentTabIndex = adminTabPane.getSelectionModel().getSelectedIndex();
-        adminTabPane.getSelectionModel().select(currentTabIndex++);
     }
 
     public void setFinance(Finance newFinance) {
