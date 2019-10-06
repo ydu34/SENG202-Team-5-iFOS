489d721b78011c05854b224ed84f2e8922c05a95
James Kwok <jtk39@uclive.ac.nz>
2019-10-06 14:25:20 +1300

    Finished creating JUnit tests for CustomerTest
    Signed-off-by: James <jtk39@uclive.ac.nz>

--- SENG202-Team-5/src/test/java/seng202/group5/information/CustomerTest.java
+++ SENG202-Team-5/src/test/java/seng202/group5/information/CustomerTest.java
@@ -22,7 +22,7 @@ class CustomerTest {
     @BeforeEach
     public void init() {
         customer = new Customer();
-        customer.setPurchasePoints(0);
+        customer.setPurchasePoints(10);
 
     }
 
@@ -30,48 +30,88 @@ class CustomerTest {
     @Test
     public void testAddPurchasePointsSmallPurchase() {
         customer.purchasePoints(smallCash);
-        assertEquals(0, customer.getPurchasePoints());
+        assertEquals(10, customer.getPurchasePoints());
     }
 
     @Disabled
     @Test
     public void testAddPurchasePointsNoPurchase() {
         customer.purchasePoints(noCash);
-        assertEquals(0, customer.getPurchasePoints());
+        assertEquals(10, customer.getPurchasePoints());
     }
 
     @Disabled
     @Test
     public void testAddPurchasePointsJustUnderPurchase() {
         customer.purchasePoints(justUnderCash);
-        assertEquals(0, customer.getPurchasePoints());
+        assertEquals(10, customer.getPurchasePoints());
     }
 
     @Disabled
     @Test
     public void testAddPurchasePointsOnePoint() {
         customer.purchasePoints(validCash);
-        assertEquals(1, customer.getPurchasePoints());
+        assertEquals(11, customer.getPurchasePoints());
     }
 
     @Disabled
     @Test
     public void testAddPurchasePointsOnePointJustUnder() {
         customer.purchasePoints(justUnderCash.plus(validCash));
-        assertEquals(1, customer.getPurchasePoints());
+        assertEquals(11, customer.getPurchasePoints());
     }
 
     @Disabled
     @Test
     public void testAddPurchasePointsTwoPoints() {
         customer.purchasePoints(validCash.multipliedBy(2).plus((smallCash)));
-        assertEquals(2, customer.getPurchasePoints());
+        assertEquals(12, customer.getPurchasePoints());
     }
 
     @Disabled
     @Test
-    public void testDiscountRemoved() {
-        customer.setPurchasePoints(10);
+    public void testDiscountRemovedSmall() {
+        Money result = customer.discount(1, validCash);
+        assertEquals(parse("NZD 9.00"), result);
+    }
+
+    @Disabled
+    @Test
+    public void testDiscountRemovedSmallPoints() {
+        Money result = customer.discount(1, validCash);
+        assertEquals(9, customer.getPurchasePoints());
+    }
+
+    @Disabled
+    @Test
+    public void testDiscountRemovedLarge() {
+        Money result = customer.discount(9, validCash);
+        assertEquals(parse("NZD 1.00"), result);
+    }
+
+    @Disabled
+    @Test
+    public void testDiscountRemovedLargePoints() {
+        Money result = customer.discount(9, validCash);
+        assertEquals(1, customer.getPurchasePoints());
+    }
+
+
+
+    @Disabled
+    @Test
+    public void testDiscountRemovedMoreThanOrder() {
+        customer.setPurchasePoints(20);
+        Money result = customer.discount(12, validCash);
+        assertEquals(parse("NZD 0.00"), result);
+    }
+
+    @Disabled
+    @Test
+    public void testDiscountRemovedMoreThanOrderPoints() {
+        customer.setPurchasePoints(20);
+        Money result = customer.discount(12, validCash);
+        assertEquals(10, customer.getPurchasePoints());
     }
 
 }
